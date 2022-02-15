6c7bb935caeca2abdfdf041b294a6f38387c3e8a
Toren Darby <darby@science.ubc.ca>
2022-02-15 12:37:06 -0800

    Remove duplication

--- src/romanNumerals.ts
+++ src/romanNumerals.ts
@@ -1,16 +1,29 @@
+class RomanNumeralSegment {
+  amount: number;
+
+  symbol: string;
+
+  constructor(amount:number, symbol: string) {
+    this.amount = amount;
+    this.symbol = symbol;
+  }
+}
+
 export default function convertToRomanNumerals(input: number): string {
   let remainder = input;
   let result = '';
 
-  if (remainder === 9) {
-    result += 'IX';
-    remainder -= 9;
-  }
+  const segments = [
+    new RomanNumeralSegment(10, 'X'),
+    new RomanNumeralSegment(9, 'IX'),
+  ];
 
-  if (remainder >= 10) {
-    result += 'X';
-    remainder -= 10;
-  }
+  segments.forEach((segment) => {
+    if (remainder >= segment.amount) {
+      result += segment.symbol;
+      remainder -= segment.amount;
+    }
+  });
 
   if (remainder === 4) {
     result += 'IV';
