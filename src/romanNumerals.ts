05852c85acadc9f28d4ef27162682e23e7104c41
Toren Darby <darby@science.ubc.ca>
2022-02-15 13:23:17 -0800

    Refactor.

--- src/romanNumerals.ts
+++ src/romanNumerals.ts
@@ -13,16 +13,20 @@ export default function convertToRomanNumerals(input: number): string {
   let remainder = input;
   let result = '';
 
+  // These have a pattern of duplication that could be removed.
   const segments = [
     new RomanNumeralSegment(1000, 'M'),
+
     new RomanNumeralSegment(900, 'CM'),
     new RomanNumeralSegment(500, 'D'),
     new RomanNumeralSegment(400, 'CD'),
     new RomanNumeralSegment(100, 'C'),
+
     new RomanNumeralSegment(90, 'XC'),
     new RomanNumeralSegment(50, 'L'),
     new RomanNumeralSegment(40, 'XL'),
     new RomanNumeralSegment(10, 'X'),
+
     new RomanNumeralSegment(9, 'IX'),
     new RomanNumeralSegment(5, 'V'),
     new RomanNumeralSegment(4, 'IV'),
